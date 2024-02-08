fdf976297fa6a338a3893f1c210a40ca354b7e77
Lucas Lessa <lessa07oliveira@gmail.com>
2024-02-08 20:25:04 -0300

    Quiz simples feito em react

---
+++ quiz-app/src/assets/data.js
@@ -0,0 +1,42 @@
+export const data = [
+  {
+    question: "Quantas empresas compõem o grupo Volkswagen?",
+    option1: "3",
+    option2: "5",
+    option3: "10",
+    option4: "7",
+    ans: 4, // A Volkswagen é composta por 7 marcas: Volkswagen, Audi, SEAT, Škoda, Bentley, Bugatti e Lamborghini.
+  },
+  {
+    question: "Quantos anos tem o grupo Volkswagen?",
+    option1: "10 anos",
+    option2: "5 anos",
+    option3: "50 anos",
+    option4: "80 anos", // O Grupo Volkswagen foi fundado em 1937, então tem mais de 80 anos.
+    ans: 4,
+  },
+  {
+    question: "Em qual bairro de São Paulo está localizado o escritório da Volkswagen?",
+    option1: "Jabaquara",
+    option2: "Tucuruvi",
+    option3: "Penha",
+    option4: "Butantã",
+    ans: 1, // O escritório da Volkswagen está localizado no bairro Jabaquara em São Paulo.
+  },
+  {
+    question: "Em qual dessas cidades está localizado o maior polo da Volkswagen?",
+    option1: "Osasco",
+    option2: "Anápolis",
+    option3: "Resende", // A cidade de Resende, no estado do Rio de Janeiro, abriga o maior polo da Volkswagen.
+    option4: "Araçatuba",
+    ans: 3,
+  },
+  {
+    question: "Em quantos países a Volkswagen atua?",
+    option1: "2 países",
+    option2: "10 países",
+    option3: "5 países",
+    option4: "7 países",
+    ans: 2, // A Volkswagen atua em mais de 10 países ao redor do mundo.
+  },
+];
